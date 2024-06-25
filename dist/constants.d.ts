f0414c3ad67afa9161059cd0fcd5606ef9656fd3
zj kkk <npckei217@gmail.com>
2024-06-25 10:49:13 +0800

    feat: update dist

---
+++ dist/constants.d.ts
@@ -0,0 +1,19 @@
+import { Percent } from '@uniswap/sdk-core';
+import JSBI from 'jsbi';
+/**
+ * @deprecated use FACTORY_ADDRESS_MAP instead
+ */
+export declare const FACTORY_ADDRESS = "0xfd8D39298478F7b88FD7687F4B470d095bb8c7Bd";
+export declare const FACTORY_ADDRESS_MAP: {
+    [chainId: number]: string;
+};
+export declare const INIT_CODE_HASH = "0x8615843ab28b4b86b2382dca22cf14f0a6ba9e52cb006531eb574042a5b54a46";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
+export declare const BASIS_POINTS: JSBI;
+export declare const ZERO_PERCENT: Percent;
+export declare const ONE_HUNDRED_PERCENT: Percent;
